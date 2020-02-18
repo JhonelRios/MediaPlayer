ffaeb42a970c19eb7cb602cfad82f9d20e4944d4
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-02-18 00:50:26 -0500

    Añadida la funcion de play y pause al boton

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const video = document.querySelector("video")
+const button = document.querySelector("button")
+
+class MediaPlayer {
+    constructor(config) {
+        this.media = config.el
+    }
+
+    play() {
+        this.media.play()
+    }
+
+    pause() {
+        this.media.pause()
+    }
+
+    togglePlay() {
+        (this.media.paused)
+            ? this.play()
+            : this.pause()
+    }
+}
+
+const player = new MediaPlayer({el: video})
+
+button.onclick = () => player.togglePlay()
