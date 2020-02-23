adab93f46268307184e8c38587ebb9c34fd2e00e
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-02-22 20:36:54 -0500

    Se agrego la pausa al tener solo el 0.25 del video en ventana, la pausa del video al selecionar otra pestaña del navegador y un serverWorker para trabajar offline

---
+++ src/plugins/AutoPause.js
@@ -0,0 +1,45 @@
+class AutoPause {
+    constructor() {
+        this.threshold = 0.25
+        this.handlerIntersection = this.handlerIntersection.bind(this)
+        this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this)
+    }
+    
+    run(player) {
+        this.player = player
+
+        const observer = new IntersectionObserver(this.handlerIntersection, {
+            threshold: this.threshold,
+        })
+
+        observer.observe(player.media)
+
+        document.addEventListener("visibilitychange", this.handlerVisibilityChange)
+    }
+
+    handlerIntersection(entries) {
+        const entry = entries[0]
+
+        const isVisible = entry.intersectionRatio >= this.threshold
+
+        if (isVisible) {
+            // Este método está siendo llamado por la clase IntersectionObserver, por lo que el "this" apunta hacia ella.
+            // Para evitar esto, le asignamos al método el mismo pero con bind a la clase AutoPause.
+            this.player.play()
+        } else {
+            this.player.pause()
+        }
+    }
+
+    handlerVisibilityChange() {
+        const isVisible = document.visibilityState === "visible"
+
+        if (isVisible) {
+            this.player.play()
+        } else {
+            this.player.pause()
+        }
+    }
+}
+
+export default AutoPause
---
+++ sw.js
@@ -0,0 +1,46 @@
+const VERSION = "v1"
+
+self.addEventListener("install", event => {
+    event.waitUntil(precache())
+})
+
+self.addEventListener("fetch", event => {
+    const request = event.request
+
+    // Validamos que la petición sea GET, ya que no nos interesa trabajar con los datos de las demás 
+    if (request.method !== "GET") {
+        return
+    }
+
+    // Buscamos en cache
+    event.respondWith(cachedResponse(request))
+
+    // Actualizamos el cache
+    event.waitUntil(updateCache(request))
+})
+
+async function precache() {
+    const cache = await caches.open(VERSION)
+    return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/src/index.js",
+        "/src/MediaPlayer.js",
+        "/src/plugins/AutoPlay.js",
+        "/src/plugins/AutoPause.js",
+        "/assets/video.mp4"
+    ])
+}
+
+async function cachedResponse(request) {
+    const cache = await caches.open(VERSION)
+    const response = await cache.match(request)
+    return response || fetch(request)
+}
+
+async function updateCache(request) {
+    const cache = await caches.open(VERSION)
+    const response = await fetch(request)
+    return cache.put(request, response)
+}
